84a87715e46c2ce64b65ac259b4ec6b8c7e8ea37
Yanis Wang <yanis.wang@gmail.com>
2014-07-12 11:30:18 +0800

    add crossorigin support

--- jserror-client.js
+++ jserror-client.js
@@ -1,10 +1,26 @@
 (function(){
-    window.onerror = function(message, file, line) {
-        pagerun.error('jserror', {
+    window.onerror = function(message, file, line, col, errorObj) {
+        var errorMessage = {
             message: message,
             file: file,
             line: line
-        });
+        };
+        if(col){
+            errorMessage.col = col;
+        }
+        if(errorObj && errorObj.stack){
+            errorMessage.stack = errorObj.stack;
+        }
+        pagerun.error('jserror', errorMessage);
         return true;
     };
+    var doc = document;
+    doc._oldCreateElement = doc.createElement;
+    doc.createElement = function(tagname){
+        var result = doc._oldCreateElement.apply(doc, arguments);
+        if(/^script$/i.test(tagname)){
+            result.setAttribute('crossorigin', true);
+        }
+        return result;
+    }
 })();
--- jserror.js
+++ jserror.js
@@ -1,6 +1,39 @@
 var fs = require('fs');
 
 module.exports = function(pagerun){
+    var bOpenUrl = false;
+    pagerun.on('proxyStart', function(msg){
+        var proxy = msg.proxy;
+        proxy.addFilter(function(httpData, next, end){
+            if(bOpenUrl === true){
+                var responseContent = httpData.responseContent;
+                if(httpData.responseCode === 200 &&
+                    responseContent !== undefined){
+                    if(httpData.responseType === 'html'){
+                        // add crossorigin
+                        responseContent = responseContent.replace(/<script(\s+[^<>]*)?>\s*<\/script>/ig,function(all, attrs){
+                            if(/src\s*=\s*['"][^'"]+/i.test(attrs) &&
+                                /['"](https?)?\/\/pagerun\//.test(attrs) === false &&
+                                /\s+crossorigin/.test(attrs) === false){
+                                all = all.replace(/<script\s+/,'$&crossorigin ');
+                            }
+                            return all;
+                        });
+                        httpData.responseContent = responseContent;
+                    }
+                }
+                // add header: access-control-allow-origin
+                var responseHeaders= httpData.responseHeaders;
+                if(responseHeaders){
+                    responseHeaders['access-control-allow-origin'] = '*';
+                }
+            }
+            next();
+        });
+    });
+    pagerun.on('webdriverOpenUrl', function(){
+        bOpenUrl = true;
+    });
     pagerun.injectCode('<script src="//pagerun/jserror.js"></script>', 'top');
     pagerun.addRequestMap('pagerun/jserror.js', {
         'responseCode': '200',
